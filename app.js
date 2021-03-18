77fe6a2d731cfc030cd57dbccd51ef38f37420c6
Dan B <dbraunby@hotmail.com>
2021-03-18 10:04:45 -0600

    command line prompts user and stores answer

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template')
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template')
+//
+//const pageHTML = generatePage(name,github);
+//
+//fs.writeFile('./index.html', pageHTML, err => {
+//    if(err) throw err;
+//
+//    console.log('Portfolio complete! index.html');
+//});
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
-
-    console.log('Portfolio complete! index.html');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
